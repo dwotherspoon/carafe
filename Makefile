d70730da8935417d932de264193b25802be18960
David Wotherspoon <david@dwotherspoon.com>
2014-10-24 22:19:14 +0100

    Some FastCGI fun

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 CFLAGS=-Wall -Wextra -Wpedantic -O0
 OBJ=main.o
 CC=gcc
-EXECNAME=bin/myapp
+EXECNAME=bin/myapp-fcgi
 
 all: main
 	@echo "Carafe built..."
 
 main: $(OBJ)
-	$(CC) $(CFLAGS) -o $(EXECNAME) $(OBJ)
+	$(CC) $(CFLAGS) -o $(EXECNAME) $(OBJ) -lfcgi
 
 clean:
 	rm -f *.out
--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include "carafe.h"
-#include "stdio.h"
-
+#include <fcgi_stdio.h>
+#include "stdlib.h"
 /* http://judy.sourceforge.net/downloads/index.html */
+/* http://www.aprelium.com/data/doc/2/abyssws-win-doc-html/cgivars.html */
+extern char **environ;
 
-int main(int argc, char* argv[]) {
-	puts("OK");
+void setup(void) {
+
+}
+
+int main(void) {
+	int i;
+	char buf[256];
+	while (FCGI_Accept() >= 0) {
+		gets(buf);
+		puts("Content-type: text/html\n\n");
+		for (i = 0; environ[i]; i++) {
+			printf("%s<br />\n", environ[i]);
+		}
+		printf("Input:<br />\n%s", buf);
+	}
+
 	return -1;
 }
